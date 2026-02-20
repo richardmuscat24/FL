c859c168f1f12ad272e1ca415e49ec12629010c4
Richard Muscat <richard.muscat.24@um.edu.mt>
2026-02-20 20:40:56 +0100

    results

--- client_app.py
+++ client_app.py
@@ -192,8 +192,8 @@ def evaluate(msg: Message, context: Context) -> Message:
         if not file_exists:
             writer.writeheader()
 
-        bank_ids = ['1677', '4', '2', '146', '4870'] #small
-        # bank_ids = ['m741', 'm1818', 'm2310','m544'] #medium 
+        # bank_ids = ['1677', '4', '2', '146', '4870'] #small
+        bank_ids = ['m741', 'm1818', 'm2310','m544'] #medium 
         
                 
         writer.writerow({
